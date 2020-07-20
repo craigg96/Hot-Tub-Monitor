6d3bbdccd78dfb472d87923a2c06a863c2f1d3d0
ongo-gablogian <61522300+ongo-gablogian@users.noreply.github.com>
2020-07-20 13:32:00 +0100

    Update Hot_Tub_Monitor.ino
    Includes power and cost metering. fixed display flickering.

--- Hot_Tub_Monitor/Hot_Tub_Monitor.ino
+++ Hot_Tub_Monitor/Hot_Tub_Monitor.ino
@@ -5,6 +5,18 @@
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+
+#define BLYNK_PRINT Serial
+double t;
+BlynkTimer timer;
+void requestTime() {
+  Blynk.sendInternal("rtc", "sync");
+}
+BLYNK_WRITE(InternalPinRTC) {
+  t = param.asDouble();  //Unix time from blynk NTP server
+}
 
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
 #define SCREEN_HEIGHT 32 // OLED display height, in pixels
@@ -16,17 +28,8 @@
 #define ONE_WIRE_BUS D3
 
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
-
-  
-#include <ESP8266WiFi.h>
-#include <BlynkSimpleEsp8266.h>
-
-// You should get Auth Token in the Blynk App.
-// Go to the Project Settings (nut icon).
+ 
 char auth[] = "I6cEXCSWkCGpOx54E05v0P8RBV5ibEOf";
-
-// Your WiFi credentials.
-// Set password to "" for open networks.
 char ssid[] = "TALKTALK580025";
 char pass[] = "38E7YE86";
 
@@ -36,6 +39,17 @@ int Tamb;
 double Irms;
 int POWER;
 int display_toggle;
+double kWh = 0;
+float kWhBuffer = 0;
+float currentMillis = 1;
+float prevMillis = 0;
+float cost = 0; //£s
+float bufferMillis=0;
+float bufferT=0;
+float currentT=1;
+float CostPerUnit = 12.8; //pence per kWh
+float bufferSize = 300000; //ms to hold buffer
+int done = 0;   //first loop indicator
 
 // Setup a oneWire instance to communicate with any OneWire device
 OneWire oneWire(ONE_WIRE_BUS);  
@@ -48,22 +62,27 @@ DallasTemperature sensors(&oneWire);
 BLYNK_WRITE(V3) { //Display Toggle
   display_toggle = param.asInt(), DEC;
 }
-
+BLYNK_WRITE(V5) {
+CostPerUnit = param.asDouble();
+}
+BLYNK_WRITE(V10){  //button to reboot, to reset cost counter. dirty but what you gonna do
+  if (param.asInt()==1){
+    delay(3000);
+    ESP.restart();
+    delay(5000);
+  }
+}
 
 void setup(void)
 {
-  
-  // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
-  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3C for 128x32
+  Serial. begin(9600);
+    
+    if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3C for 128x32
     Serial.println(F("SSD1306 allocation failed"));
     for(;;); // Don't proceed, loop forever
   }
   // Clear the buffer
   display.clearDisplay();
-  // display.display() is NOT necessary after every single drawing command,
-  // unless that's what you want...rather, you can batch up a bunch of
-  // drawing operations and then update the screen all at once by calling
-  // display.display(). These examples demonstrate both approaches...
 
   display.setTextSize(1);             // Normal 1:1 pixel scale
   display.setTextColor(SSD1306_WHITE);        // Draw white text
@@ -99,7 +118,7 @@ void setup(void)
 //  Serial.begin(9600);
  
   emon1.current(A0, 111.1);             // Current: input pin, calibration.
-
+  timer.setInterval(1000L, requestTime); //interrupt to update RTC every second
 
 }
 
@@ -108,18 +127,42 @@ void setup(void)
 void loop(void)
 { 
   Blynk.run();
+  timer.run();
+
+  currentT=t;
+  
+  if(done==0 && t>5){  //execute the first loop after RTC value has updated. 0 value for buffer clock
+    bufferT=t;
+    bufferMillis=currentMillis;
+    kWhBuffer = 0;
+    done=1;
+    Serial.print("done - buffert= ");
+    Serial.println(bufferT);
+  }
 
   Irms = emon1.calcIrms(1480);
+  if (Irms < 0.2 || millis() < 25000){
+    Irms = 0;
+  }
+  Irms*=3.22; //multiplier including power factor.
 
-  if (Irms < 0.5){
-    Irms = 0;
-  }
-  
-  if (millis() < 12000){
-    Irms = 0;
-  }
 
   POWER = 230*Irms;
+    prevMillis = currentMillis;
+    currentMillis = millis();
+  kWhBuffer+=(currentMillis-prevMillis)*POWER*0.001*0.00027778*0.001; //0.001=millis to s, 0.0002778=Ws to WH (1/3600), 0.001=WH to kWh
+ 
+  
+  if((currentMillis - bufferMillis) > bufferSize && done==1){  //5 minutes since buffer cleared
+    cost+=(CostPerUnit*(kWhBuffer*((t-bufferT)/(currentMillis-bufferMillis)))*0.01*1000); //correction for millis drift over 5 mins. 0.01= #s to p, 1000 = seconds to milliseconds
+    kWh+=((kWhBuffer*((t-bufferT)/(currentMillis-bufferMillis)))*1000);
+     
+    bufferT=t;
+    bufferMillis = currentMillis;  //reset buffer timers
+    kWhBuffer = 0; 
+  }
+
+   
   
   // Send the command to get temperatures
   sensors.requestTemperatures(); 
@@ -134,9 +177,10 @@ void loop(void)
   Blynk.virtualWrite(V1, Tamb); //sending to Blynk
   Blynk.virtualWrite(V2, Irms); //sending to Blynk
   Blynk.virtualWrite(V4, POWER); //sending to Blynk
+  Blynk.virtualWrite(V6, cost + (CostPerUnit*0.01*kWhBuffer));  //includes buffered estimate, which will be scrubbed when time is updated
+  Blynk.virtualWrite(V7, kWh + kWhBuffer);
 
   display.clearDisplay();
-  display.display();
 
 if (display_toggle == 1){
 
@@ -157,10 +201,11 @@ if (display_toggle == 1){
   display.print(Tamb);
   display.println(" C");
 
+
+  display.print(kWh+kWhBuffer,2);
+  display.print(" kWh   $ ");
+  display.print(cost+(kWhBuffer*CostPerUnit*0.01),2);
   
-  display.print("Current = ");
-  display.print(Irms);
-  display.println(" Amps");
 
   display.display();
 
@@ -185,5 +230,6 @@ void testdrawcircle(void) {
     delay(1);
   }
 
+  
   delay(1000);
 }
